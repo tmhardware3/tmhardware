705e53a7f5dba6ea68ce2f858a4d0a6a18f0b2c8
tmhardware3 <tmhardware3@gmail.com>
2026-07-09 19:24:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener("scroll",()=>{
+
+const header=document.querySelector("header");
+
+
+if(window.scrollY>50){
+
+header.style.background="#000";
+
+}else{
+
+header.style.background="#111";
+
+}
+
+
+});
